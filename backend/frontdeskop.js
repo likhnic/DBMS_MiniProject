9da25c295cced2b471abef19181dee79c96d0bff
likhnic <likhith26090@gmail.com>
2023-02-27 22:44:03 +0530

    Added Venkat's FrontDeskOp

--- backend/frontdeskop.js
+++ backend/frontdeskop.js
@@ -51,7 +51,7 @@ app.post("/api/frontdeskop/register", async (req, res) => {
     catch(error){
         console.log(error);
         res.status(404).json({
-            success: 'Error in registering patient'
+            error: 'Error in registering patient'
         });
     }
 });
@@ -61,24 +61,30 @@ app.post("/api/frontdeskop/register", async (req, res) => {
 app.post("/api/frontdeskop/:id", async (req, res) => {
     const {id} = req.params;
     const {AppointmentID, StartTime, EndTIme, ExaminationRoom, PatientAadhar, DocID} = req.body;
-    let sql = "select * from appointment where AppointmentID = " + AppointmentID;
+    try {
+        let sql = "select * from appointment where AppointmentID = " + AppointmentID;
+        let result = await client.query(sql);
+        if (result.rows.length > 0) {
+            console.log("Appointment already exists");
+            res.status(404).json({
+                error: "Appointment already exists"
+            })
+            return;
+        }
 
-    let result = await client.query(sql);
-    if (result.rows.length > 0) {
-        res.status(404).json({
-            error: "Appointment already exists"
-        })
-        console.log("Appointment already exists");
-        return;
+        sql = "insert into appointment (AppointmentID, StartTime, EndTime, ExaminationRoom, PatientAadhar, DocID) values (" + AppointmentID + ", '" + StartTime + "', '" + EndTIme + "', " + ExaminationRoom + ", " + PatientAadhar + ", " + DocID + ")";
+        
+        result = await client.query(sql);
+        console.log("Appointment created successfully");
+        res.status(200).json({
+            success: 'Appointment created successfully'
+        });
     }
-
-    sql = "insert into appointment (AppointmentID, StartTime, EndTime, ExaminationRoom, PatientAadhar, DocID) values (" + AppointmentID + ", '" + StartTime + "', '" + EndTIme + "', " + ExaminationRoom + ", " + PatientAadhar + ", " + DocID + ")";
-    
-    result = await client.query(sql);
-    console.log("Appointment created successfully");
-    res.status(200).json({
-        success: 'Appointment created successfully'
-    });
+    catch {
+        res.status(404).json({
+            error: 'Error in creating appointment'
+        });
+    }
 });
 
 
@@ -88,62 +94,58 @@ app.put("/api/frontdeskop/:id", async (req, res) => {
     const {StayID, StartTime, RoomNo, PatientAadhar} = req.body;
 
     if (id != PatientAadhar) {
-        res.json({
-            code: 404,
-            message: "Patient not found"
-        })
         console.log("Patient not found");
+        res.status(404).json({
+            error: "Patient not found"
+        });
         return;
     }
-
-    let sql = "select * from stay where StayID = " + StayID;
-    await client.query(sql, async (err, result) => {
-        if (err) throw err;
+    try {
+        let sql = "select * from stay where StayID = " + StayID;
+        let result  = await client.query(sql);
         if (result.rows.length > 0) {
-            res.json({
-                code: 404,
-                message: "Stay already exists"
-            })
             console.log("Stay already exists");
+            res.status(404).json({
+                error: "Stay already exists"
+            });
             return;
         }
-    });
 
-    sql = "insert into stay (StayID, StartTime, RoomNo, PatientAadhar) values (" + StayID + ", '" + StartTime + "', " + RoomNo + ", " + PatientAadhar + ")";
-    client.query(sql, (err, result) => {
-        if (err) throw err;
+        sql = "insert into stay (StayID, StartTime, RoomNo, PatientAadhar) values (" + StayID + ", '" + StartTime + "', " + RoomNo + ", " + PatientAadhar + ")";
+        result = await client.query(sql);
         console.log("Stay created successfully");
-        res.json({
-            'code': 200,
-            'message': 'Stay created successfully'
+        res.status(200).json({
+            success: 'Stay created successfully'
         });
-    });
+    }
+    catch {
+        res.status(404).json({
+            error: 'Error in creating stay'
+        });
+    }
 });
 
 // removing the patient from the patient table and update the end time of patient (patient, stay tables)
 app.delete("/api/frontdeskop/:id", async (req, res) => {
     const {id} = req.params;
     console.log("id = " + id);
-    let sql = "select * from patient where Aadhar = " + id;
-    await client.query(sql, async (err, result) => {
-        if (err) throw err;
+    try {
+        let sql = "select * from patient where Aadhar = " + id;
+        let result = await client.query(sql);
         if (result.rows.length == 0) {
-            res.json({
-                code: 404,
-                message: "Patient not found"
-            })
             console.log("Patient not found");
+            res.status(404).json({
+                error: "Patient not found"
+            });
             return;
         }
+
         sql = "delete from patient where Aadhar = " + id;
-        await client.query(sql, async (err, result) => {
-            if (err) throw err;
-            console.log("Patient removed successfully");
-            res.json({
-                'code': 200,
-                'message': 'Patient removed successfully'
-            });
-        });
+        result = await client(sql);
+        console.log("Patient removed successfully");
+        // res.status(200).json({
+        //     success: 'Patient removed successfully'
+        // });
 
         // update the end time of the patient in the stay table
         var today = new Date();
@@ -151,16 +153,18 @@ app.delete("/api/frontdeskop/:id", async (req, res) => {
         var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
         var dateTime = date+' '+time;
         sql = "update stay set endtime = '" + dateTime + "' where stayid = (select stayid from stay where stay.patient = " + id + " order by starttime desc limit 1)";
+        result = client.query(sql);
 
-        client.query(sql, (err, result) => {
-            if (err) throw err;
-            console.log("End time updated successfully");
-            res.json({
-                'code': 200,
-                'message': 'End time updated successfully'
-            });
+        console.log("End time updated successfully");
+        res.status(200).json({
+            success: 'End time updated successfully'
         });
-    });
+    }
+    catch {
+        res.status(404).json({
+            error: 'Error in removing patient'
+        });
+    }
 });
 
 app.listen(3000, () => {
