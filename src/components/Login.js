2bb8b5c87e179518531bcfc0cd1e9d81035e1dcf
likhnic <likhith26090@gmail.com>
2023-03-01 21:51:09 +0530

    Added Basic

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+        <div className="container">
+            <div className="row">
+                <div className="card">
+                    Login
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Login
