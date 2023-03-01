0a7905a61c0e8a749b0f455f3361ed73e698104f
shashankgoud002 <101133749+shashankgoud001@users.noreply.github.com>
2023-03-01 22:31:06 +0530

    Changed all functions

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -25,15 +25,15 @@ const type_IDs = {
     '3': 'AdminID'
 }
 
-router.delete("/user/:id/:type", async (req, res) => {
-    // if (req.user.id != req.params.id) {
+router.delete("/user/:ID/:type", async (req, res) => {
+    // if (req.user.ID != req.params.ID) {
     //     return res.json({ error: "You are not authorized to do this operation!" })
     // }
-    const id = req.params.id
+    const ID = req.params.ID
     const type_index = req.params.type
-    if (type_index.localeCompare('2') == 0) {
-        let sqlQuery1 = 'UPDATE User SET status = 0 where ID = ' + id
-        let sqlQuery2 = 'UPDATE Doctor SET isWorking = 0 where ID = ' + id;
+    if (type_index == 2) {
+        let sqlQuery1 = `UPDATE User SET Status = 0 where ID = ${ID}`
+        let sqlQuery2 = `UPDATE Doctor SET isWorking = 0 where ${type_IDs[type_index]} = ${ID}`
         try {
             let result1 = await query(sqlQuery1);
             let result2 = await query(sqlQuery2);
@@ -45,8 +45,10 @@ router.delete("/user/:id/:type", async (req, res) => {
             return res.json({ error: error });
         }
     } else {
-        let sqlQuery1 = 'DELETE FROM ' + types[type_index] + ' WHERE ' + type_IDs[type_index] + '= ' + id
-        let sqlQuery2 = 'DELETE FROM User WHERE ID = ' + id
+        let sqlQuery1 = 'DELETE FROM ' + types[type_index] + ' WHERE ' + type_IDs[type_index] + '= ' + ID
+        let sqlQuery2 = 'DELETE FROM User WHERE ID = ' + ID
+        console.log(sqlQuery1)
+        console.log(sqlQuery2)
         try {
             let result1 = await query(sqlQuery1);
             let result2 = await query(sqlQuery2);
@@ -62,33 +64,41 @@ router.delete("/user/:id/:type", async (req, res) => {
 
 
 router.post("/user", async (req, res) => {
-    // if (req.user.id != req.params.id) {
+    // if (req.user.ID != req.params.ID) {
     //     return res.json({ error: "You are not authorized to do this operation!" })
     // }
-    const { username, type_index,Aadhar, password } = req.body
-    password_confirm = req.body['password-confirm']
+    const { ID, type_index,Aadhar, Password, Password_confirm,Name,Phone,Address,Position} = req.body
+    
     console.log(req.body);
-    let sqlQuery1 = 'SELECT ' + type_IDs[type_index] + ' FROM ' + types[type_index] + ' WHERE ' + type_IDs[type_index] + ' = ' + username
+    let sqlQuery1 = 'SELECT ' + type_IDs[type_index] + ' FROM ' + types[type_index] + ' WHERE ' + type_IDs[type_index] + ' = ' + ID
     console.log(sqlQuery1)
     try {
         let result1 = await query(sqlQuery1);
         if (result1.length > 0) {
             return res.status(409).json({
-                error: "username already exists"
+                error: "ID already exists"
             })
-        } else if (password !== password_confirm) {
+        } else if (Password !== Password_confirm) {
             return res.status(400).json({
                 error: "password and confirm password do not match"
             })
         }else{
-            let hashedPassword = await bcrypt.hash(password, 8)
-            let sqlQuery2 = 'INSERT INTO ' + 'User' + ' values (' + username + ','+Aadhar+',"' + hashedPassword + '",1)'
+            let hashedPassword = await bcrypt.hash(Password, 8)
+            let sqlQuery2 = 'INSERT INTO ' + 'User' + ' values (' + ID + ','+Aadhar+',"' + hashedPassword + '",1)'
+            if(type_index==2)
+                sqlQuery3 = `INSERT INTO ${types[type_index]} values (${ID},"${Position}","${Name}","${Phone}","${Address}",1)`
+            else
+                sqlQuery3 = `INSERT INTO ${types[type_index]} values (${ID},"${Name}","${Phone}","${Address}")`
+
+            // ID name phone address
             console.log(sqlQuery2)
-            
+            console.log(sqlQuery3)
             try {
                 let result2 = await query(sqlQuery2);
+                let result3 = await query(sqlQuery3);
                 return res.json({
-                    result2: result2
+                    result2: result2,
+                    resutl3: result3
                 })
             } catch (error) {
                 console.log(error);
@@ -101,6 +111,30 @@ router.post("/user", async (req, res) => {
     }
 })
 
+router.post('/login', async (req, res) => {
+
+    const { ID, Password } = req.body
+
+    let sqlQuery = `SELECT * FROM User WHERE ID = ${ID} AND Status=1;`
+    console.log(sqlQuery)
+    try {
+        const result = await query(sqlQuery);
+        console.log(result.length)
+        if(result.length == 0){
+            return res.json({error: "Invalid Credentials"});
+        }
+        const comPass = bcrypt.compare(Password, result[0].Password)
+        if(!comPass){
+            return res.json({error: "Invalid Credentials"});
+        }
+        // const token = jwt.sign({user: {id: result.rows[0].ID}}, "secrethaha")
+        // return res.json({user: token})
+        return res.json({success:"successfully logged in"})
+    } catch (error) {
+        console.log(error);
+        return res.json({error: error});
+    }
+})
 
 router.get('/', (req, res) => {
     res.json({ success: "Website is live!" })
