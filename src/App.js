6910f28c43af8071abc6af1e78ac0691775d5d84
Venkatasai-102 <venkatasaikgp102@gmail.com>
2023-03-01 23:45:25 +0530

    card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Login from './components/Login'
 import Navbar from './components/Navbar'
+import Card from './components/Card'
 import {
     BrowserRouter as Router,
     Routes,
@@ -18,6 +19,12 @@ const App = () => {
                     <Route exact path='/admin' element={<Admin_Dashboard/>}/>
                 </Routes>
             </Router>
+            <Card
+                url="#"
+                img_url="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp"
+                card_text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure dignissimos, itaque quia laboriosam debitis enim reprehenderit voluptas placeat? Id, inventore. Rerum laboriosam suscipit cum sunt nemo, asperiores harum aspernatur velit cupiditate tempora expedita nihil unde eius officia impedit, aliquam ut fugiat qui libero."
+                name="John Doe"
+            />
         </>
     )
 }
